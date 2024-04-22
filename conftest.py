ef7f2a0903c44fdadeca65c309bf1488daed74a0
Елена <e.lapteva@etpgpb.ru>
2024-04-22 21:36:08 +0300

    update conftest.py add requirements.txt

--- conftest.py
+++ conftest.py
@@ -3,6 +3,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
 def driver():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--window-size=1440, 1080")
     service = Service(executable_path=ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     yield driver
