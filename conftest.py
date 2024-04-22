bd7ce6796c1aca773ca1b8aac83b0b1282eb773c
Елена <e.lapteva@etpgpb.ru>
2024-04-22 22:29:30 +0300

    add fixture add pytest.txt

--- conftest.py
+++ conftest.py
@@ -1,11 +1,15 @@
+import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
+
+@pytest.fixture
 def driver():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--incognito")
-    chrome_options.add_argument("--window-size=1440, 1080")
+    chrome_options.add_argument("--tb=short")
+    chrome_options.add_argument("--window-size=1440,1080")
     service = Service(executable_path=ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     yield driver
