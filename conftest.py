3b117bbac1149f64e9040674b2b5bd0bf98f5061
Елена <e.lapteva@etpgpb.ru>
2024-05-06 22:03:37 +0300

    fix chrome_options

--- conftest.py
+++ conftest.py
@@ -1,16 +1,17 @@
 import pytest
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
 def driver():
     chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=500,500")
     chrome_options.add_argument("--incognito")
     chrome_options.add_argument("--tb=short")
-    chrome_options.add_argument("--window-size=1440,1080")
     service = Service(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     yield driver
     driver.quit()
